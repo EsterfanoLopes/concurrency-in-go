439fe37ea130f8e3b7a36fad1763021033abd8f3
Esterfano Lopes <esterfano.lopes@gmail.com>
2022-06-15 18:48:05 +0200

    Lecture 40. Setting variables to sleeping barber

--- sleeping_barber/sleeping_barber.go
+++ sleeping_barber/sleeping_barber.go
@@ -1,15 +1,43 @@
 package sleeping_barber
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+var (
+	seatingCapacity = 10
+	arrivalRate     = 100 // miliseconds
+	cutDuration     = 1000 * time.Millisecond
+	timeOpen        = 10 * time.Second
+)
 
 func Run() {
 	// seed random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 	// create data structure for the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
 
 	// add barbers
 
