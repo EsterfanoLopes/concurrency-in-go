3cc6e28a7af8b243e4a00a41a5ea6d89f3b7535c
Esterfano Lopes <esterfano.lopes@gmail.com>
2023-05-04 18:31:46 +0200

    fix

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-<<<<<<< Updated upstream
 import (
-	"concurrency-in-go/dining_philosophers"
+	"concurrency-in-go/producer_consumer"
 )
-=======
-import "concurrency-in-go/producer_consumer"
->>>>>>> Stashed changes
 
 func main() {
 	// waitgroupcases.WaitgroupRun()
@@ -15,17 +11,5 @@ func main() {
 
 	producer_consumer.ProducerConsumerRun()
 
-<<<<<<< Updated upstream
-	dining_philosophers.Run()
-=======
 	// dining_philosophers.Run()
-
-	// simple_channels.Run()
-
-	// channel_select.Run()
-
-	// buffered_channels.Run()
-
-	// sleeping_barber.Run()
->>>>>>> Stashed changes
 }
